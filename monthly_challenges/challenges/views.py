49671f63a34af7e3f54d1a84803e50a44289b1e8
biraj094 <koiralabiraj@gmail.com>
2021-06-09 22:24:05 +0545

    Add typecasting in routes and redirect to a route

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseNotFound
+from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
 # Create your views here.
 
+monthly_challenge_dict = {
+    'january':'Jan: 100 pushups!',
+    'february':'Feb: 100 pushups!',
+    'march':'Mar: 100 pushups!',
+    'april':'Apr: 100 pushups!',
+    'may':'May: 100 pushups!',
+    'june':'Jun: 100 pushups!',
+    'july':'July: 100 pushups!',
+    'august':'Aug: 100 pushups!',
+    'september':'Sept: 100 pushups!',
+    'october':'Oct: 100 pushups!',
+    'november':'Nov: 100 pushups!',
+    'december':'Dec: 100 pushups!',
+
+}
+
+def monthly_challenge_by_number(request,month):
+    months = list(monthly_challenge_dict.keys())
+    if month==0 or month > len(months):
+        return HttpResponseNotFound('Invalid month')
+    redirect_link = months[month-1]
+    return HttpResponseRedirect('/challenges/'+redirect_link)
 
 def monthly_challenge(request, month):
-    response = ''
-    if month == 'january':
-        response = 'Jan : 100 puhsup!'
-    elif month == 'february':
-        response = 'Feb : 200 skips!'
-    else :
+    try:
+        response = monthly_challenge_dict[month] 
+    except:
         return HttpResponseNotFound('Month entered is not available !')
     return HttpResponse(response)
