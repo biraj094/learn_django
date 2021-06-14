c4c9e6a68ef8dc51c5df06bed7b3f192cb83728c
biraj094 <koiralabiraj@gmail.com>
2021-06-14 15:21:25 +0545

    Add all url and view functions for the blog app

---
+++ biraj_website/blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [path('', views.homepage, name="homepage"),
+               path('posts', views.posts, name="posts-page"),
+               path('posts/<slug:slug>', views.post, name="post-page")
+               ]
--- biraj_website/blog/views.py
+++ biraj_website/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def homepage(request):
+    return HttpResponse('Hello! Welcome to the homepage.')
+
+
+def posts(request):
+    return HttpResponse('This is the all posts page')
+
+
+def post(request,slug):
+    return HttpResponse('This is individual post page ' + slug)
