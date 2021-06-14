90a8c46d98bdfb3b958c8c095adde14da9fb9c95
biraj094 <koiralabiraj@gmail.com>
2021-06-14 17:34:42 +0545

    Add all templates for the view functions

--- biraj_website/blog/views.py
+++ biraj_website/blog/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse('Hello! Welcome to the homepage.')
+    return render(request,'blog/homepage.html')
+    # return HttpResponse('Hello! Welcome to the homepage.')
 
 
 def posts(request):
